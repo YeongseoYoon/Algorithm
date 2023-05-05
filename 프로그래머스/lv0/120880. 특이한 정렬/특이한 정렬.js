8201e58fec07a1de8b5d55d3eba11a27ae1e0980
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-05-06 04:59:41 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,10 @@
+function solution(numlist, n) {
+
+    return numlist.sort((a,b) => {
+        if(Math.abs(a-n) === Math.abs(b-n)){
+            return b-a;
+        }
+        return Math.abs(a-n) - Math.abs(b-n);
+    })
+
+}
