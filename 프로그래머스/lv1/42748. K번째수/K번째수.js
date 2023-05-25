20373ad3e236eb35273654d4a82cff8f8e058f25
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-05-25 20:41:21 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,9 +1,25 @@
+const bubbleSort = arr => {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+  	noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+
+        noSwaps = false;
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+}
+
 function solution(array, commands) {
     let answer = [];
     
     for(let i =0; i<commands.length;i++){
         let arr1 = array.slice(commands[i][0]-1, commands[i][1]);
-        arr1.sort((a,b)=> a-b);
+        bubbleSort(arr1);
         answer.push(arr1[commands[i][2]-1]);
     }
     
