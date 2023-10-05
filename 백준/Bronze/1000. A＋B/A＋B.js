5bd3b4bcbfcaec64856828987f330fc6712e4937
윤영서 <86523545+YeongseoYoon@users.noreply.github.com>
2023-10-05 09:30:40 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "window" ? "./example.txt" : "./dev/stdin" ;
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+const solution = (a, b) => {
+  console.log(parseInt(a, 10) + parseInt(b, 10));
+};
+
+solution(input[0], input[1]);
