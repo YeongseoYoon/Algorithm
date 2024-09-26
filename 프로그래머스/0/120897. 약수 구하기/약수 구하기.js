1b48e2b2f538daedb5f4f9077ee43a03aee715fa
윤영서 <86523545+YeongseoYoon@users.noreply.github.com>
2024-09-26 21:49:15 +0900

    [level 0] Title: 약수 구하기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let divisors = [];
+    
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            divisors.push(i);
+            if (i !== n / i) {
+                divisors.push(n / i);
+            }
+        }
+    }
+    
+    return divisors.sort((a, b) => a - b);
+}
