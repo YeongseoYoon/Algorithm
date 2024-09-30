9ec0d248073aa71126e36362afb9ded39a75eedf
윤영서 <86523545+YeongseoYoon@users.noreply.github.com>
2024-09-30 20:33:41 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,15 +1,7 @@
 function solution(n) {
-    let sum = 0;
-    if(n % 2 === 1){
-        for (let i = 1; i <= n; i += 2) {
-            sum += i;
-        }
+    if (n % 2 === 1) {  
+        return Math.pow((n + 1) / 2, 2);
+    } else { 
+        return (n * (n + 1) * (n + 2)) / 6;
     }
-    if(n % 2 === 0){
-        for (let i = 0; i <= n; i += 2) {
-            sum += i*i;
-        }
-    }
-    
-    return sum;
 }
