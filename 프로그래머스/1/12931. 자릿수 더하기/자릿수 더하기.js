5de82fee29ea376a0f47266c7c540a764b86c0a7
윤영서 <86523545+YeongseoYoon@users.noreply.github.com>
2025-09-06 21:32:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,3 +1,4 @@
-function solution(n) {
-    return (n+'').split('').reduce((acc, curr) => Number(acc) + Number(curr), 0);
+function solution(n)
+{
+    return (""+n).split('').reduce((acc, curr) => Number(acc)+Number(curr),0);
 }
